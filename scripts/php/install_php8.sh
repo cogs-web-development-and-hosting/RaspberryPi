c473a184e568f6139a30c542ad7c7d86870ec60c
Aaron Jones <ajones061392@yahoo.com>
2022-08-19 21:42:29 -0700

    Moved counting.java to misc, added php8 install script
    Moved Counting.java to misc, and added a php8 install script for Raspian. Need to organize my folders better but will do so in time.

--- scripts/php/install_php8.sh
+++ scripts/php/install_php8.sh
@@ -2,10 +2,30 @@
 # php8.0 and above on Raspberry Pi's Raspbian.
 
 #! bin/bash
- 
+
 # Need to install third party repositories for Raspian
 # Firstly must ensure that lsb-release is installed.
 
-#Update the package 
+#Update the package
 echo "Updating package listing"
 sudo apt update
+
+#Ensuring lsb-release is installed
+echo "Installing lsb-release..."
+sudo apt install lsb-release
+
+#Installing GPG Key for this PHP repository to our Raspberry Pi
+echo "Installing GPG Key and adding it to keyring"
+curl https://packages.sury.org/php/apt.gpg | sudo tee /usr/share/keyrings/suryphp-archive-keyring.gpg >/dev/null
+
+#Create a source file that points to the repository
+echo "Creating source file to the repository"
+echo "deb [signed-by=/usr/share/keyrings/suryphp-archive-keyring.gpg] https://packages.sury.org/php/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
+
+#Now update repositories
+echo "Updating Repositories.."
+sudo apt update
+
+#Installing PHP 8.1
+echo "Install PHP 8.1"
+sudo apt install php8.1-cli
